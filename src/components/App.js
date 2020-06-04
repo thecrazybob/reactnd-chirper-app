e59ca2ed77ef22446ede4d63a3395675979eab95
Mohammed Sohail <itscrazybob@gmail.com>
2020-06-04 16:53:12 +0400

    Added Dashboard component and Provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { receiveInitialData } from '../actions/shared'
+import Dashboard from './Dashboard';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(receiveInitialData())
+  }
   render() {
     return (
       <div>
         Starter Code
+        {this.props.loading === true ? null : <Dashboard />}
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = ({authedUser}) => {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/components/Dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+    render() {
+        return(
+        <div>
+            <h3 className='center'>Your Timeline</h3>
+            <ul className='dashboard-list'>
+            {
+            this.props.tweetIds.map(
+                (id) => 
+                (
+                <li key={id}>
+                    <div>{id}</div>
+                </li>
+                )
+            ) }
+            </ul>
+        </div>
+        )
+    }
+}
+
+const mapStateToProps = ({tweets}) => {
+    return {
+        tweetIds: Object.keys(tweets)
+        .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)
